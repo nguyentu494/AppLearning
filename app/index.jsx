4b7d47ad371416a8db172ed41606d43a49f4c2d0
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-09-13 17:15:43 +0700

    create app e-learning project

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="bg-slate-400">
+      <Text >ABC</Text>
+    </View>
+  );
+}
+
+
